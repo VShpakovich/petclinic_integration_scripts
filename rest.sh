6cb1a935f9601f8591c5a28e3a876d89ac32726a
Helena Celejowska <120586825+helacel@users.noreply.github.com>
2024-04-10 15:31:15 +0200

    Update rest.sh

--- rest.sh
+++ rest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo y | rm -r ${HOME}/backend # Remove the backend directory if it exists
+
 # Defining environment variables moved as parameters
 DATABASE_IP=$1
 DATABASE_PORT=$2
@@ -7,17 +9,17 @@ DATABASE_USER=$3
 DATABASE_PASSWORD=$4
 VM_PORT=$5
 
+APP_PROP="src/main/resources/application.properties"
+MYSQL_PROP="${HOME}/backend/spring-petclinic-rest/src/main/resources/application-mysql.properties"
+
 # Creating directory for backend
-mkdir -p "home/studentadmin/backend"
-cd "home/studentadmin/backend"
+mkdir -p "${HOME}/backend"
+cd "${HOME}/backend"
 
 # Installing necessary packages
 echo "Installing necessary packages..."
-sudo apt update -y
-sudo apt install -y mysql-server
-sudo apt install -y openjdk-11-jdk
-
-
+sudo apt-get update -y
+sudo apt-get install -y default-jdk mysql-server
 
 # Cloning the Spring PetClinic repository
 echo "Cloning Spring PetClinic repository..."
@@ -26,17 +28,17 @@ cd spring-petclinic-rest
 
 # Modifying application.properties to use MySQL instead of HSQLDB
 echo "Configuring application.properties..."
-sed -i "s/=hsqldb/=mysql/g" ./src/main/resources/application.properties
-sed -i "s/9090/${VM_PORT}/g" ./src/main/resources/application.properties
+sed -i -n "s/=hsqldb/=mysql/g" ${APP_PROP}
+sed -i -n "s/9966/${VM_PORT}/g" ${APP_PROP}
 
-sed -i "s/jdbc:hsqldb/jdbc:mysql/g" ./src/main/resources/application-mysql.properties
-sed -i "s/localhost/${DATABASE_IP}/g" ./src/main/resources/application-mysql.properties
-sed -i "s/3306/${DATABASE_PORT}/g" ./src/main/resources/application-mysql.properties
-sed -i "s/pc/${DATABASE_USER}/g" ./src/main/resources/application-mysql.properties
-sed -i "s/petclinic/${DATABASE_PASSWORD}/g" ./src/main/resources/application-mysql.properties
+sed -i -n "s/jdbc:hsqldb/jdbc:mysql/g" ${MYSQL_PROP}
+sed -i -n "s/localhost/${DATABASE_IP}/g" ${MYSQL_PROP}
+sed -i -n "s/3306/${DATABASE_PORT}/g" ${MYSQL_PROP}
+sed -i -n "s/pc/${DATABASE_USER}/g" ${MYSQL_PROP}
+sed -i -n "s/pc/${DATABASE_PASSWORD}/g" ${MYSQL_PROP}
 
 # Building and running the Spring Boot application
 echo "Building and running the Spring Boot application..."
-./mvnw spring-boot:run
+./mvnw spring-boot:run &
 
 echo "Backend server started successfully."
