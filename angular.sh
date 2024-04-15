b9bdf5e0f0d5b31262781b3848ff6d13ea3218f1
Helena Celejowska <120586825+helacel@users.noreply.github.com>
2024-04-15 22:23:34 +0200

    Update angular.sh

--- angular.sh
+++ angular.sh
@@ -16,15 +16,16 @@ cd ~/
 
 # Install package manager for Node.js (nvm)
 echo "Installing nvm..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+curl -fsSL https://deb.nodesource.com/setup_18.x  | sudo -E bash -
+sudo apt-get install -y nodejs
 
-nvm install 14.11.1
+
+# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# nvm install v18.13.0
 
 # Cloning the Spring PetClinic repository
 git clone https://github.com/spring-petclinic/spring-petclinic-angular.git
-npm install --save-dev @angular/cli@latest
 cd spring-petclinic-angular
 
 # Modifying environment.prod.ts to use the backend IP and port
@@ -33,6 +34,7 @@ sed -i "s/8080/${BACKEND_PORT}/g" src/environments/environment.ts src/environmen
 
 # Building and running the Angular application
 echo "Building and running the Angular application..."
+cd ~
 echo N | npm install -g @angular/cli@latest
 echo N | npm install
 echo N | ng analytics off
